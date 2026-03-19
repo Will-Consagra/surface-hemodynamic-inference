f7ac879866057a356d32720da18d93d0c7c75cc0
Will-Consagra <wxc5756@rit.edu>
2026-03-19 13:57:19 -0400

    first commit

---
+++ ModelTraining/train_lik_emul_1param.py
@@ -0,0 +1,293 @@
+##conda activate neuro_proc
+import numpy as np 
+import math
+import torch 
+import torch.nn as nn
+from torch.special import gammaln, xlogy
+from torch.utils.data import DataLoader, Dataset
+import zuko 
+
+import scipy
+from scipy.fft import fft 
+from scipy.special import sph_harm, legendre, gamma
+from torch.distributions.multivariate_normal import MultivariateNormal
+
+import os 
+from scipy.io import loadmat
+
+import matplotlib.pyplot as plt
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#### Forward Model HRF Kernel ####
+## constants
+DT = 0.001
+P_DELAY = 6.0
+UNDERSHOOT = 16.0
+P_DISP = 1.0
+U_DISP = 1.0
+P_U_RATIO = 0.167
+
+## double gamma HRF model
+A_1 =  torch.tensor([P_DELAY / P_DISP - 1])
+A_2 = torch.tensor([UNDERSHOOT / U_DISP - 1])
+C = torch.tensor([P_U_RATIO])
+
+class HRF(nn.Module):
+	def __init__(self, t_r=1.0, T=100, L=30, onset=0.0):
+		super().__init__()
+		self.T = T
+		self.t_r = t_r 
+		self.dur = L * t_r
+		self.onset = onset
+		self.L = L
+	def _gamma_pdf(self, t, a, b):
+		## General gamma PDF: (b^(a+1) * t^a * exp(-b*t)) / Gamma(a+1)
+		eps = 1e-8  # avoid t=0 instability
+		t = torch.clamp(t, min=eps)
+		norm = torch.exp(gammaln(a + 1))
+		return (b ** (a + 1)) * (t**a) * torch.exp(-b * t) / norm
+	def _double_gamma_hrf(self, theta):
+		t = torch.linspace(0, self.dur, int(float(self.dur) / DT), device=theta.device, dtype=theta.dtype) - float(self.onset) / DT
+		t = t[::int(self.t_r/DT)]
+		peak = self._gamma_pdf(t, A_1, theta)
+		undershoot = self._gamma_pdf(t, A_2, theta)
+		hrf = peak - C * undershoot
+		return hrf, t
+	def _construct_toeplitz(self, kernel):
+		"""
+			Construct the Toeplitz matrix for a 1D convolution kernel.
+
+			Args:
+				kernel (torch.Tensor): The 1D convolution kernel of shape (L,).
+			Returns:
+				torch.Tensor: The Toeplitz matrix of shape (signal_length, signal_length).
+		"""
+		toeplitz_matrix = torch.zeros((self.T, self.T + self.L - 1))
+		for i in range(self.T):
+			toeplitz_matrix[i, i:i + self.L] = kernel
+		return toeplitz_matrix.T[:self.T,:]
+	def forward(self, theta):
+		hrf_kernels, _ = self._double_gamma_hrf(theta)
+		H = torch.stack([self._construct_toeplitz(k) for k in hrf_kernels])
+		return H  
+
+def link(theta, min_theta, max_theta):
+	u = (theta - min_theta)/(max_theta - min_theta)
+	return torch.distributions.Normal(0,1).icdf(u)
+
+def inv_link(theta_tilde, min_theta, max_theta):
+	u = torch.distributions.Normal(0, 1).cdf(theta_tilde)
+	return min_theta + (max_theta - min_theta) * u
+
+def np2freq(sigs):
+	sigs_ft = torch.fft.rfft(sigs, dim=-1)
+	sigs_real = sigs_ft.real
+	sigs_complex = sigs_ft.imag
+	sigs_freq = torch.stack([sigs_real, sigs_complex], dim=1)
+	return sigs_freq
+
+class Simulator:
+	def __init__(self, M, t_r=1.0, L=30, onset=0.0, t1_min = 5, t1_max = 7, 
+				 t2_min = 0.5, t2_max = 1.5, lam_min = 0.05, lam_max = 0.1,
+				   amin = 0.5, amax = 1.0, sigma_e = 2.5e-2):
+		super().__init__()
+		#### Measurement Parameters ####
+		self.M = M
+		self.t_r=t_r ##T_r of the acquisition 
+		self.hrf_model = HRF(t_r=t_r, T=M, L=L, onset=onset)   
+		#### Hierarchical Model ####
+		self.t1_min = t1_min
+		self.t1_max = t1_max
+		## Map range within the 99% quantile of normal
+		self.eps_1 = 0.01 / 0.98 * (t1_max - t1_min) 
+		self.p_theta_tilde_1 = scipy.stats.norm(loc=0, scale=1)
+		self.p_lambda = scipy.stats.uniform(loc=lam_min, scale=lam_max-lam_min)
+		self.p_amp = scipy.stats.uniform(loc=amin, scale=amax-amin)
+		self.sigma_e = sigma_e
+	def simulate_neural_signals(self, lambdas):
+		Nsamples = len(lambdas)
+		signal_map = {}
+		for ns in range(Nsamples):
+			lambda_rate_ns = lambdas[ns]
+			t = 0
+			event_times = []; amplitudes = []
+			while t < (self.M*self.t_r):
+				# Draw the next inter-arrival time from Exponential(lambda)
+				dt = np.random.exponential(scale=1/lambda_rate_ns)
+				at = self.p_amp.rvs(1).item()
+				t += dt
+				if t < (self.M*self.t_r):
+					event_times.append(t)
+					amplitudes.append(at)
+			signal_map[ns] = (np.array(event_times), np.array(amplitudes))
+		return signal_map
+	def theta_inv_link(self, theta_tildes):
+		theta_1 = inv_link(theta_tildes[:,0], self.t1_min - self.eps_1, self.t1_max + self.eps_1)
+		return theta_1.view(-1,1)
+	def simulate(self, Nsamples):
+		## simulate latents 
+		theta_tildes = torch.from_numpy(self.p_theta_tilde_1.rvs(Nsamples)).float().view(-1,1)
+		thetas = self.theta_inv_link(theta_tildes)
+		lambdas = self.p_lambda.rvs(Nsamples)
+		signal_map = self.simulate_neural_signals(lambdas) 
+		discretized_neural_signals = np.zeros((Nsamples, self.M))
+		for ns in range(Nsamples):
+			spike_times_ns = signal_map[ns][0]; spike_amps_ns = signal_map[ns][1];
+			if spike_times_ns.size > 0:
+				for t, a_t in zip(spike_times_ns, spike_amps_ns):
+					t_idx = int(t // self.t_r)
+					discretized_neural_signals[ns, t_idx] = discretized_neural_signals[ns, t_idx] + a_t 
+		discretized_neural_signals = torch.from_numpy(discretized_neural_signals).float()
+		## simulate observed signals 
+		H_theta = self.hrf_model(thetas)
+		y_true = torch.matmul(H_theta, discretized_neural_signals.unsqueeze(-1)).squeeze(-1)
+		y_obs = y_true + self.sigma_e*torch.normal(0, 1, size=(Nsamples, self.M))
+		return y_obs, y_true, theta_tildes, discretized_neural_signals
+
+## learn summary network (approximate marginal posterior mean S(y) \approx E[theta|y]
+class MLP(nn.Module):
+	def __init__(self, input_channels, input_size, hidden_sizes, output_size, activation=nn.ReLU(), dropout=0.0):
+		super().__init__()
+		layers = []
+		# Add the first linear layer to handle input channels
+		layers.append(nn.Linear(input_channels * input_size, hidden_sizes[0]))
+		layers.append(activation)
+		if dropout > 0.0:
+			layers.append(nn.Dropout(dropout))
+		for i in range(1, len(hidden_sizes)):
+			layers.append(nn.Linear(hidden_sizes[i - 1], hidden_sizes[i]))
+			layers.append(activation)
+			if dropout > 0.0:
+				layers.append(nn.Dropout(dropout))
+		layers.append(nn.Linear(hidden_sizes[-1], output_size))
+		self.mlp = nn.Sequential(*layers)
+	def forward(self, x):
+		x = x.view(x.size(0), -1)
+		return self.mlp(x) 
+
+SUMNETDIR = "../PreTrainedModels/models/summary_nets_1param"
+LIKEMULDIR = "../PreTrainedModels/models/lik_emul_1param"
+fname_summary_net_state_dict = os.path.join(SUMNETDIR, "posterior_mean_summary_statistic_shifted_hrf.pth")
+
+M = 1200; TR = 0.72 ## HCP-parameters 
+burn_in = 100
+M_record = M; ## get to 'steady state'
+M_tot = M_record + burn_in; 
+start_m = M_tot-M_record
+t_r = TR
+L = 30
+onset = 0.0
+
+## HCP-calibration 
+t1_min, t1_max = 0.5, 2.5
+gamma_hat = {'sigma_e': 0.15150529651120925,
+			 'amin': 0.7435368830394751,
+			 'amax': 0.8372886914947318,
+			 'lam_min': 0.0039519025340883215,
+			 'lam_max': 0.2167510244193856
+			 }
+
+sigma_e = gamma_hat["sigma_e"]
+amin, amax = gamma_hat["amin"], gamma_hat["amax"]
+lam_min, lam_max = gamma_hat["lam_min"], gamma_hat["lam_max"]
+
+sim_model = Simulator(M_tot,
+					t_r=t_r, 
+					  L=L, 
+					  onset=onset,
+					  t1_min = t1_min, 
+					  t1_max = t1_max, 
+					  lam_min = lam_min, 
+					  lam_max = lam_max,
+					   amin = amin, 
+					  amax = amax, 
+					  sigma_e = sigma_e)
+
+## evaluation 
+np.random.seed(0)
+N_test = 200
+Npost_samples = 5000
+
+y_eps_test, y_true_test, theta_tildes_test, neural_signals_test = sim_model.simulate(N_test)
+y_eps_test = y_eps_test[:,start_m:]
+y_true_test = y_true_test[:,start_m:]
+neural_signals_test = neural_signals_test[:,start_m:]
+theta_test = sim_model.theta_inv_link(theta_tildes_test)
+y_eps_test = (y_eps_test - y_eps_test.mean(dim=-1).view(-1,1))/(y_eps_test.std(dim=-1).view(-1,1))
+## FT 
+y_test_eps_freq = np2freq(y_eps_test)
+
+## Summay Statistic 
+p = 1
+input_channels = 2 ## real + complex channels
+input_size = M_record//2 + 1
+hidden_sizes = [M_record, M_record//2, M_record//4]
+activation = nn.ReLU()
+dropout = 0.0
+summary_network = MLP(input_channels, 
+					  input_size, 
+					  hidden_sizes, 
+					  p, 
+					  activation=activation, dropout=dropout)
+summary_network.load_state_dict(torch.load(fname_summary_net_state_dict)["model_state_dict"])
+summary_network = summary_network.to(device)
+summary_network.eval()
+for param in summary_network.parameters():
+    param.requires_grad = False
+
+
+## Likelihood Emulator
+lik_emul = zuko.flows.NSF(p, p, transforms=5, hidden_features=[64] * 3)
+lik_emul = lik_emul.to(device)
+
+## opimization 
+Nbatch = 100
+learning_rate = 1e-5
+optim_lik = torch.optim.Adam(params=lik_emul.parameters(), 
+								lr=learning_rate)
+
+## early stopping 
+num_epochs = 500000
+patience = 5 
+best_test_loss = float("inf")
+epochs_without_improvement = 0
+
+# Training loop
+for epoch in range(num_epochs):
+	## noisy BOLD  
+	batch_y_eps, batch_y_true, batch_theta_tildes, batch_neural_signals = sim_model.simulate(Nbatch)
+	batch_y_eps = batch_y_eps[:,start_m:]
+	batch_y_eps = (batch_y_eps - batch_y_eps.mean(dim=-1).view(-1,1))/(batch_y_eps.std(dim=-1).view(-1,1))
+	## FT 
+	batch_y_eps_freq = np2freq(batch_y_eps)
+	## S(y)
+	batch_S_y = summary_network(batch_y_eps_freq.to(device))
+	## log-likelihood 
+	log_lik = lik_emul(batch_theta_tildes.to(device)).log_prob(batch_S_y)
+	## gradient of likelihood 
+	loss = -log_lik.mean()
+	optim_lik.zero_grad()
+	loss.backward()
+	optim_lik.step()
+	if epoch % 10000 == 0:
+		print(f"Epoch {epoch}, Loss: {loss.item():.4f}")
+		with torch.no_grad():    
+			test_S_y = summary_network(y_test_eps_freq.to(device))
+			test_loss = -lik_emul(theta_tildes_test.to(device)).log_prob(test_S_y).mean()
+			# eval 
+			print(f"Epoch {epoch}, Test Loss: {test_loss.item():.4f}")
+			# inf 
+			test_S_y_samples = lik_emul(theta_tildes_test.to(device)).sample((Npost_samples,))
+			test_S_y_errors = torch.mean((test_S_y_samples.mean(dim=0) - test_S_y)**2,dim=0)
+			tl1 = test_S_y_errors[0].item()
+		fname_lik_model_state_dict = os.path.join(LIKEMULDIR, "likelihood_emulator_posterior_mean_summary_shifted_hrf_epoch_%s.pth" % epoch)
+		torch.save({
+					"epoch": epoch,
+					"test_neg_lik":test_loss.item(),
+					"test_losses": tl1,
+					"model_state_dict": lik_emul.state_dict(),
+					"optimizer_state_dict": optim_lik.state_dict(),
+					}, fname_lik_model_state_dict)
+
+
